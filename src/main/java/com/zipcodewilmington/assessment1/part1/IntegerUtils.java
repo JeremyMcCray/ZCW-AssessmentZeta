3f64633c15c1eb42b6f3ab163528d9bbb35eed05
Jeremy McCray <jmccray@zipcodes-MacBook-Pro.local>
2020-02-17 10:50:42 -0500

    Movin on up

--- src/main/java/com/zipcodewilmington/assessment1/part1/IntegerUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/IntegerUtils.java
@@ -11,7 +11,11 @@ public class IntegerUtils {
      * @return the sum of all integers between 0 and not including `n`
      */
     public static Integer getSumOfN(Integer n) {
-        return null;
+        Integer ans = 0;
+        for (int i = 0; i <=n ; i++) {
+            ans+=i;
+        }
+        return ans;
     }
 
     /**
@@ -19,14 +23,26 @@ public class IntegerUtils {
      * @return the product of all integers between 0 and not including `n`
      */
     public static Integer getProductOfN(Integer n) {
-        return null;
+        Integer ans = 1;
+        for (int i = 1; i <=n ; i++) {
+            ans*=i;
+        }
+        return ans;
     }
+
+
 
     /**
      * @param val integer value input by client
      * @return integer with identical digits in the reverse order
      */
     public static Integer reverseDigits(Integer val) {
-        return null;
+       String stringVal = val.toString();
+       String ans = "";
+        for (int i = stringVal.length()-1; i >= 0 ; i--) {
+            ans+=stringVal.charAt(i);
+        }
+        Integer ans1 = Integer.parseInt(ans);
+        return ans1;
     }
 }
