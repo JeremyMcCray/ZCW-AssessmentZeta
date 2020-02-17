2a755c2864fec7ebf79ac260a593b865227279b1
Jeremy McCray <jmccray@zipcodes-MacBook-Pro.local>
2020-02-17 11:00:29 -0500

    To the east side.

--- src/main/java/com/zipcodewilmington/assessment1/part1/RockPaperSissorsEvaluator.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/RockPaperSissorsEvaluator.java
@@ -13,7 +13,15 @@ public class RockPaperSissorsEvaluator {
      * @return the respective winning move
      */
     public String getWinningMove(String handSign) {
-        return null;
+        String ans = "";
+        if(handSign.equals("rock")){
+            ans="paper";
+        } else if(handSign.equals("scissor")) {
+            ans="rock";
+        } else if(handSign.equals("paper")){
+            ans="scissor";
+        }
+        return ans;
     }
 
     /**
@@ -21,7 +29,15 @@ public class RockPaperSissorsEvaluator {
      * @return the respective losing move
      */
     public String getLosingMove(String handSign) {
-        return null;
+        String ans = "";
+        if(handSign.equals("rock")){
+            ans="scissor";
+        } else if(handSign.equals("scissor")) {
+            ans="paper";
+        } else if(handSign.equals("paper")){
+            ans="rock";
+        }
+        return ans;
     }
 
     /**
@@ -30,6 +46,15 @@ public class RockPaperSissorsEvaluator {
      * @return a string representative of the winning hand sign between the two players
      */
     public String getWinner(String handSignOfPlayer1, String handSignOfPlayer2) {
-        return null;
+
+        String ans = "";
+        if(handSignOfPlayer1.equals("rock") && handSignOfPlayer2.equals("scissor")){
+            ans="rock";
+        } else if(handSignOfPlayer1.equals("scissor") && handSignOfPlayer2.equals("paper")) {
+            ans="scissor";
+        } else if(handSignOfPlayer1.equals("paper") && handSignOfPlayer2.equals("rock")){
+            ans="paper";
+        }
+        return ans;
     }
 }
