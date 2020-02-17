64463c952758b6e0aab81b11105ab98a4a9394c4
Jeremy McCray <jmccray@zipcodes-MacBook-Pro.local>
2020-02-17 12:37:14 -0500

    We finally got a piece of the pie.

--- src/main/java/com/zipcodewilmington/assessment1/part2/ArrayUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part2/ArrayUtils.java
@@ -11,7 +11,13 @@ public class ArrayUtils {
      * Given an array of objects, named `objectArray`, and an object `objectToCount`, return the number of times the `objectToCount` appears in the `objectArray`
      */
     public static Integer getNumberOfOccurrences(Object[] objectArray, Object objectToCount) {
-        return null;
+        Integer ans = 0;
+        for (Object ele:objectArray) {
+            if(ele.equals(objectToCount)){
+                ans++;
+            }
+        }
+        return ans;
     }
 
     /**
@@ -20,8 +26,22 @@ public class ArrayUtils {
      * @return an array with identical content excluding the specified `objectToRemove`
      * Given an array of objects, name `objectArray`, and an object `objectToRemove`, return an array of objects with identical contents excluding `objectToRemove`
      */
-    public static Object[] removeValue(Object[] objectArray, Object objectToRemove) {
-        return null;
+    public static Integer[] removeValue(Object[] objectArray, Object objectToRemove) {
+        int counter = 0;
+        for (Object ele: objectArray) {
+            if(!ele.equals(objectToRemove)){
+                counter++;
+            }
+        }
+        Integer[] ans = new Integer[counter];
+        int stepper = 0;
+        for (int i = 0; i < objectArray.length; i++) {
+            if(!objectArray[i].equals(objectToRemove)){
+                ans[stepper] = (Integer)objectArray[i];
+                stepper++;
+            }
+        }
+        return ans;
     }
 
     /**
@@ -30,7 +50,25 @@ public class ArrayUtils {
      * given an array of objects, named `objectArray` return the most frequently occuring object in the array
      */
     public static Object getMostCommon(Object[] objectArray) {
-        return null;
+        int count = 1,tempCount;
+        Object mostCommon = objectArray[0];
+        for (int i = 0; i <objectArray.length-1 ; i++) {
+            Object commonChecked = objectArray[i];
+            tempCount = 0;
+            for(int j = 1; j <objectArray.length;j++){
+                if(commonChecked.equals(objectArray[j])){
+                    tempCount++;
+                }
+                if(tempCount>count){
+                    mostCommon = commonChecked;
+                    count= tempCount;
+                }
+            }
+        }
+
+
+
+        return mostCommon;
     }
 
 
@@ -40,7 +78,25 @@ public class ArrayUtils {
      * given an array of objects, named `objectArray` return the least frequently occuring object in the array
      */
     public static Object getLeastCommon(Object[] objectArray) {
-        return null;
+        int count = 10,tempCount;
+        Object leastCommon = objectArray[0];
+        for (int i = 0; i <objectArray.length-1 ; i++) {
+            Object commonChecked = objectArray[i];
+            tempCount = 0;
+            for(int j = 0; j < objectArray.length;j++){
+                if(commonChecked.equals(objectArray[j])){
+                    tempCount++;
+                }
+                if(tempCount<count){
+                    leastCommon = commonChecked;
+                    count= tempCount;
+                }
+            }
+        }
+
+
+
+        return leastCommon;
     }
 
     /**
@@ -49,7 +105,19 @@ public class ArrayUtils {
      * @return an array containing all elements in `objectArray` and `objectArrayToAdd`
      * given two arrays `objectArray` and `objectArrayToAdd`, return an array containing all elements in `objectArray` and `objectArrayToAdd`
      */
-    public static Object[] mergeArrays(Object[] objectArray, Object[] objectArrayToAdd) {
-        return null;
+    public static Integer[] mergeArrays(Object[] objectArray, Object[] objectArrayToAdd) {
+        int counter = 0;
+        Integer[] ans = new Integer[objectArray.length + objectArrayToAdd.length];
+
+        for (Object ele: objectArray) {
+            ans[counter] = (Integer)ele;
+            counter++;
+        }
+        for (Object ele: objectArrayToAdd) {
+            ans[counter] = (Integer)ele;
+            counter++;
+        }
+
+        return ans;
     }
 }
