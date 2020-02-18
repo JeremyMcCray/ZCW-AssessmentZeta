e4ba47e5ecd0e052d1903ac90997846863a84e8f
Jeremy McCray <jmccray@zipcodes-MacBook-Pro.local>
2020-02-18 15:49:50 -0500

    all done :D thanks dave kelly

--- src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
+++ src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
@@ -1,8 +1,19 @@
 package com.zipcodewilmington.assessment1.part5;
 
+import static com.zipcodewilmington.assessment1.part1.BasicStringUtils.reverse;
+
 public class Palindrome {
 
     public Integer countPalindromes(String input) {
-        return null;
+        Integer counter = 0;
+        for (int i = 0; i <= input.length() ; i++) {
+            for (int j = i+1; j <= input.length() ; j++) {
+                if(input.substring(i,j).equals(reverse(input.substring(i,j)))){
+                    counter++;
+                }
+
+            }
+        }
+        return counter;
     }
 }
